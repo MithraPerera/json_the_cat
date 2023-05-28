ac141f6148f7fd78d9a47bc2310f76ab99af615a
Mithra Perera <mithraperera@hotmail.com>
2023-05-28 15:22:55 -0400

    Improved code modularity and added tests with mocha and chai

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,26 @@
 const request = require('request');
 
-const userRequestedBreed = process.argv[2];
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    // // Edge Case: Request Failed
+    // if (error) return console.log(error);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${userRequestedBreed}`, (error, response, body) => {
-  // Edge Case: Request Failed
-  if (error) return console.log(error);
+    // // console.log(typeof body); // returns string
 
-  // console.log(typeof body); // returns string
+    // // Convert string to JSON object
+    const data = JSON.parse(body);
 
-  // Convert string to JSON object
-  const data = JSON.parse(body);
+    // // Edge Case: Breed not found
+    // if (data.length === 0) {
+    //   console.log("Breed not found.");
+    //   return;
+    // }
 
-  // Edge Case: Breed not found
-  if (data.length === 0) {
-    console.log("Breed not found.");
-    return;
-  }
+    // // console.log(data);
+    // // console.log(typeof data); // Returns object
+    // console.log(data[0].description);
+    callback(error, data[0] ? data[0].description : null);
+  });
+};
 
-  // console.log(data);
-  // console.log(typeof data); // Returns object
-  console.log(data[0].description);
-});
+module.exports = { fetchBreedDescription };
